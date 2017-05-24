a24395f0827b92d42f8a00c20dd0c3404fa55657
Andrey Zubritskiy <andreizubritskiy@gmail.com>
2017-05-24 20:40:44 +0300

    Added event component

--- src/pages/events-map/events-map.ts
+++ src/pages/events-map/events-map.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'events-map',
@@ -8,12 +9,16 @@ export class EventsMapPage {
   lat = 50.45466;
   lng = 30.5238;
   events = [];
-  
-  constructor() {
+
+  constructor(private navController: NavController) {
     this.events = [{
       name: 'Event1',
       longtitude: 30.5238,
       latitude: 50.45466
     }];
+  }
+
+  goToEvent (event) {
+    //this.navController.push(['/app', 'event', event.$key]);
   }
 }
